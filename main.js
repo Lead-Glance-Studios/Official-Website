5d04702ec6c000ab4699fea6842c868472f05416
MSAkel <akel.ms93@gmail.com>
2020-01-20 20:58:20 -0500

    Landing page mobile view

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", init)
+
+function init() {
+  document.querySelector(".box").addEventListener("click", ev => 
+  document.querySelector(".box").classList.toggle("x"))
+}
