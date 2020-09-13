7765c60cef25372f15f3af8a16efe26a2b2187a8
MSAkel <akel.ms93@gmail.com>
2020-09-13 01:53:41 -0400

    updated work section projects display functionality. Updated styling and content of sections

--- main.js
+++ main.js
@@ -14,58 +14,42 @@ function eventListeners(){
 function createProjectsList(){
   let projectsList = [
     {
+      image: "./imgs/placeholder.png",
       number: "01",
       title: "title of project"
     },
     {
+      image: "http://lorempixel.com/200/200/fashion/",
       number: "02",
       title: "title of project"
     },
     {
+      image: "http://lorempixel.com/200/200/sports/",
       number: "03",
       title: "title of project"
     },
   ]
 
-  let ul = document.getElementById("projects-list");
+  let container = document.querySelector(".projects-list");
   
 
   projectsList.forEach(item => {
-    let li = document.createElement("li");
+    let project = document.createElement("div");
+    let image = document.createElement("img")
     let number = document.createElement("p");
     let title = document.createElement("h5");
 
+    image.src = item.image
     number.textContent = item.number;
     title.textContent = item.title;
 
-    li.appendChild(number);
-    li.appendChild(title);
-    ul.appendChild(li);
+    let textContainer = document.createElement("div")
+    textContainer.classList.add("text-container")
+
+    textContainer.appendChild(number);
+    textContainer.appendChild(title);
+    project.appendChild(image);
+    project.appendChild(textContainer);
+    container.appendChild(project);
   })
-}
-
-function projectsScroll(){
-  const slider = document.querySelector('#projects-list');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', ev => {
-  isDown = true;
-  startX = ev.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-});
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-});
-slider.addEventListener('mousemove', ev => {
-  if(!isDown) return;
-  ev.preventDefault();
-  const x = ev.pageX - slider.offsetLeft;
-  const scrollSpeed = (x - startX) * 1;
-  slider.scrollLeft = scrollLeft - scrollSpeed;
-});
 }
